d665fddbdf73dceca451e4a353c9c1b3e56f2337
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-06-15 15:57:13 +0530

    :sleepy: going back again!!

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<BrowserRouter><App /><BrowserRouter/>);
+  const { getByText } = render(<App/>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
