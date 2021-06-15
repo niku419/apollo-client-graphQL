e05e87166fe0fd45a6a19b2de6f3ad9f27f231e7
Allam Nikhil Sai <2019198@iiitdmj.ac.in>
2021-06-15 15:32:02 +0530

    :sleepy: updated App.test.js to build

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<BrowserRouter><App /><BrowserRouter/>);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
